e970299cd8b692f5bdaf14579f1a7dde8c6324a3
Tanya Agrahari <rg96958227@gmail.com>
2025-09-02 18:19:26 +0530

    controlled component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,35 @@
 import { useState } from 'react'
 
 function App() {
-  const [ val, setVal]= useState("Tanya agrahari")
+  const [name , setName ]= useState("");
+  const [password , setPassword ]= useState("");
+  const [email , setEmail ]= useState("");  
+
   return (
 
       
       <div>
-        <h1>get Input field value </h1>
-        <input type="text"  value={val} onChange={(event)=> setVal(event.target.value)} placeholder=" enter user name " />
-        <h1>{val}</h1>
-        <button onClick={ ()=> setVal("")} > clear value </button>
+        <h1>contoller component </h1>
+        <form action="">
+          <input  value={name } onChange={(event)=> setName(event.target.value)} type="text"placeholder='Enter name ' />
+        <br />
+        <br />
+          <input  value={password} onChange={(event)=> setPassword(event.target.value)} type="password"placeholder='Enter password ' />
+        <br />
+        <br />
+          <input  value={email} onChange={(event)=> setPassword(event.target.value)} type="text"placeholder='Enter email ' />
+        <br />
+        <br />
 
+        <button>submit </button>
+       <button onClick={()=> {setEmail('') ; setName(''); setName('')}}>clear</button>
+
+        </form>
+        <h3>{name}</h3>
+       
+        <h3>{password}</h3>
+       
+        <h3>{email}</h3>
 
       </div>
   );
