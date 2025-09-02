5868c413a280778154fc04aed79cede95dd7a842
Tanya Agrahari <rg96958227@gmail.com>
2025-09-02 20:30:47 +0530

    more about checkbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import { useState } from 'react'
-import Skills from './Skills'
+import { useState } from "react";
 
 function App() {
+  const [gender, setGender] = useState(" ");
 
 
   return (
 
       
       <div>
-        <h1>handle checkBox in react </h1>
-        <Skills/>
+        <h1> Handle radio and dropdown </h1>
+        <h3>select gender</h3>
+        <input onChange={(e)=>setGender(e.target.value)} type="radio" name="gender" id="male" value="male" /> 
+       <lebel htmlFor="male">Male</lebel>
+       <br />
+        <input onChange={(e)=>setGender(e.target.value) }type="radio" name="gender" id="female" value="female"/> female
+       <lebel htmlFor="female">female</lebel>
+       <br />
+       <h2>selected gender : {gender}</h2>
         
       </div>
   );
